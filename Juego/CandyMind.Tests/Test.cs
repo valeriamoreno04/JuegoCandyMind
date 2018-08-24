c917d38f6ea700aa4f927715068474d4ef28579c
Valeria Moreno <valeria.morenoz@upb.edu.co>
2018-08-24 14:27:34 -0500

    All game files

---
+++ Juego/CandyMind.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CandyMind.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
