4ba443be3031811734c81af457f06ecab39732c4
Andrés Felipe Cano <andresfe98@gmail.com>
2018-08-27 00:07:23 -0500

    primera versión del juego
    Phaser 2, todo se hizo en una copia de los archivos originales para no perderlos, las funciones también se guardaron en una copia, para verlo usar la ruta:
    /Juego/CandyMind/juegoTemporal.html

--- Juego/CandyMind/javascript/main.js
+++ Juego/CandyMind/javascript/main.js
@@ -1,58 +1,90 @@
-﻿window.onload = function()
-{
-   const c = document.getElementById('canvas');
-   c.width = window.innerWidth;
-   c.height = window.innerHeight;
+var mainState = {
+  preload: function(){
+    //load our player
+    game.load.image('alien', 'Recursos/PantallaJuego/Alien1.png');
 
-   const ctx = c.getContext('2d');
+    //loading the pipe
+    game.load.image('pipe', 'Recursos/PantallaJuego/Obstaculos1.png');
+  },
 
-   const environment = new Environment(c, ctx);
-   const bird = new Bird(250, 300, ctx);
-   const pipes = [];
-   let pipeSet = generateRandomPipes(ctx, c.width, c.height);
-   pipes.push(pipeSet.top, pipeSet.bottom);
-   setInterval(function()
-   {
-     let pipeSet = generateRandomPipes(ctx, c.width, c.height);
-     pipes.push(pipeSet.top, pipeSet.bottom);
-    }, 2600);
-   gameLoop();
+  create: function(){
+    game.stage.backgroundColor  = '#71c5cf';
 
-function gameLoop(){
-     bird.update(pipes);
-     if (!bird.dead)
-     {
-       environment.update();
+    //start game physics
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 
-       pipes.forEach(function(pipe1)
-       {
-         pipe1.update();
-       });
-     }
-     environment.render();
-     pipes.forEach(function(pipe1){
-       pipe1.render();
-     });
-     bird.render();
-     if (bird.dead)
-     {
-       drawGameOver(ctx, c);
-     }
-     window.requestAnimationFrame(gameLoop);
-   }
- };
+    this.alien = game.add.sprite(100, 245, 'alien');
 
-function generateRandomPipes(ctx, canvasWidth, canvasHeight){
-   let lengthTop = Math.round(Math.random()*200+50);
-   let lengthBottom = canvasHeight - 200 - lengthTop;
-   let returnVal = { };
-   returnVal.top = new Pipe(canvasWidth, -5, lengthTop, 4, ctx);
-   returnVal.bottom = new Pipe(canvasWidth, canvasHeight+5-lengthBottom, lengthBottom, 4, ctx);
-   return returnVal;
- }
+    //apply physics to Alien
+    game.physics.arcade.enable(this.alien);
 
-function drawGameOver(ctx, c){
-  ctx.font="30px Verdana";
-  ctx.textAlign="center";
-  ctx.fillText("Game Over!!",c.width/2 , c.height/2);
-}
+    this.alien.body.gravity.y = 1000;
+
+    //jump
+    var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    spaceKey.onDown.add(this.jump, this);
+
+    this.pipes = game.add.group();
+
+    this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+    this.score = 0;
+
+    this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff"});
+  },
+
+  update: function(){
+    //restar gmae if alien goes above or below screen
+    if(this.alien.y < 0 || this.alien.y > 490){
+      this.restartGame();
+    }
+
+    game.physics.arcade.overlap(this.alien, this.pipes, this.restartGame, null);
+  },
+
+  jump: function(){
+    this.alien.body.velocity.y = -350;
+  },
+
+  restartGame: function(){
+    game.state.start('main');
+  },
+
+  addOnePipe: function(x, y){
+    var pipe = game.add.sprite(x, y, 'pipe');
+
+    this.pipes.add(pipe);
+
+    game.physics.arcade.enable(pipe);
+
+    //making pipe move left
+    pipe.body.velocity.x = -200;
+
+    //destroy pipe when out of screen
+    pipe.checWorldBounds = true;
+    pipe.outOfBoundsKill = true;
+
+  },
+
+  addRowOfPipes: function(){
+    //random number 1-5 for hole position
+    var hole = Math.floor(Math.random()*5) + 1;
+
+    //add pipes with one hole
+    for(var i = 0; i < 8; i++){
+      if(i  != hole && i != hole + 1){
+        this.addOnePipe(400, i * 60 + 10);
+      }
+    }
+
+    this.score += 100;
+    this.labelScore.text = this.score;
+  }
+
+};
+
+var game = new Phaser.Game(400, 490);
+
+game.state.add('main', mainState);
+
+game.state.start('main');
